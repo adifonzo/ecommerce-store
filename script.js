864f293bb314a09da9ccf9436807e32af8fbfc6b
aidenjdifonzo <aiden.difonzo@gmail.com>
2022-11-01 19:59:45 -0400

    added a sale page with a countdown timer until black friday

--- script.js
+++ script.js
@@ -6,4 +6,29 @@ hamburger.addEventListener('click', function () {
     navMenu.classList.toggle('active');
 });
 
+const daysValue = document.querySelector('#days');
+const hoursValue = document.querySelector('#hours');
+const minutesValue = document.querySelector('#minutes');
+const secondsValue = document.querySelector('#seconds');
+
+// Date I'm counting down to
+const countDownDate = new Date('Nov 25, 2022 00:00:00').getTime();
+
+const incriment = setInterval(function () {
+    const now = new Date().getTime();
+    let difference = countDownDate - now;
+
+    const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((difference % (1000 * 60)) / 1000);
+
+    daysValue.innerHTML = days;
+    hoursValue.innerHTML = hours;
+    minutesValue.innerHTML = minutes;
+    secondsValue.innerHTML = seconds;
+
+    console.log(days, hours, minutes, seconds);
+
+});
 
